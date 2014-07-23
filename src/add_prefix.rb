190d8d7c8a23d79c839df5acd0591f05d649c75d
Darren Li <darren.li@sage.com>
2014-07-22 17:43:59 -0700

    refectoring

--- src/add_prefix.rb
+++ src/add_prefix.rb
@@ -3,13 +3,11 @@ require 'open3'
 
 def add_prefix()
   file_name = ARGV[0]
-  content = ''
-  File.open(file_name , 'r') do |f|
-    content = f.read
-  end
   i, o, e = Open3.popen3('git rev-parse --abbrev-ref HEAD')
-  File.open(file_name , 'w') do |f|
+  File.open(file_name , 'r+') do |f|
+    content = f.read
     content.insert(0, "#{o.read.strip}: ")
+    f.seek(0, IO::SEEK_SET)
     f.write(content)
   end
 end
